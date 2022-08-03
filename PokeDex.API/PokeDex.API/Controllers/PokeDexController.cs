4cabf14885c07836beb5229d91ba021cd0074be1
Ana <ana.grskovic56@gmail.com>
2022-08-03 21:53:48 +0200

    Renamed .NET project, added a controller and a contracts project.

---
+++ PokeDex.API/PokeDex.API/Controllers/PokeDexController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PokeDex.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/pokemon")]
+    public class PokeDexController
+    {
+        [HttpGet("all", Name = "GetAllPokemon")]
+        public IActionResult GetAll()
+        {
+            return null;
+        }
+    }
+}
